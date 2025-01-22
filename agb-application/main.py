1b854f88f8aa626ca6f13a52cb075c48ea6af3a9
a.dolgov <dolgov_am@mail.ru>
2025-01-22 17:53:40 +0300

    fixed parameter

--- agb-application/main.py
+++ agb-application/main.py
@@ -16,4 +16,7 @@ def main():
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app",
+                host=settings.run.host,
+                port=settings.run.port,
+                reload=True)
