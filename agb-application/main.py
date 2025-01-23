80d23e384f1fee1428f3f22bc7d5359d4100ceb4
a.dolgov <dolgov_am@mail.ru>
2025-01-23 10:27:22 +0300

    bug fixed

--- agb-application/main.py
+++ agb-application/main.py
@@ -1,22 +1,33 @@
+from contextlib import asynccontextmanager
+from core.models import db_helper
 import uvicorn
 from fastapi import FastAPI
 from api import router as api_router
 from core.config import settings
 
-app = FastAPI()
-app.include_router(
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    print("dispose engine")
+    await db_helper.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
+main_app.include_router(
     api_router,
     prefix=settings.api.prefix
 )
 
 
-@app.get('/')
+@main_app.get('/')
 def main():
     return {"success": "true"}
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app",
+    uvicorn.run("main:main_app",
                 host=settings.run.host,
                 port=settings.run.port,
                 reload=True)
