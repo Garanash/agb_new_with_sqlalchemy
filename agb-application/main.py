0970b5f99401fd3dc697cf056b38b979b5bbddd1
a.dolgov <dolgov_am@mail.ru>
2025-01-24 16:15:27 +0300

    create start & fake_start UI

--- agb-application/main.py
+++ agb-application/main.py
@@ -1,11 +1,13 @@
 from contextlib import asynccontextmanager
 from core.models import db_helper
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import ORJSONResponse
 from api import router as api_router
 from core.config import settings
+from fastapi.templating import Jinja2Templates
 
+templates = Jinja2Templates(directory="templates")  # регистрируем папку как папку с шаблонами джинджа
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -23,8 +25,8 @@ main_app.include_router(
 
 
 @main_app.get('/')
-def main():
-    return {"success": "true"}
+def main(request: Request):
+    return templates.TemplateResponse('fake_start.html', {'request': request})
 
 
 if __name__ == '__main__':
