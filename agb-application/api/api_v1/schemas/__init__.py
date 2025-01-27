f7e90390c0e1c38d13f13f32aaa51f069a9b2e30
a.dolgov <dolgov_am@mail.ru>
2025-01-27 09:40:39 +0300

    fix init schemas file

--- agb-application/api/api_v1/schemas/__init__.py
+++ agb-application/api/api_v1/schemas/__init__.py
@@ -2,10 +2,26 @@ __all__ = (
     "UserBase", "UserRead", "UserUpdatePartial", "UserDelete", 'UserUpdate', "UserCreate",
     "MetizBase", "MetizDelete", "MetizCreate", "MetizUpdatePartial", "MetizUpdate", "MetizRead", "BaseModel",
     "RWDBase", "RWDRead", "RWDUpdate", "RWDCreate", "RWDDelete", "RWDUpdatePartial",
-    "ProjectBase", "ProjectRead", "ProjectUpdate", "ProjectUpdatePartial", "ProjectCreate", "ProjectDelete"
+    "ProjectBase", "ProjectRead", "ProjectUpdate", "ProjectUpdatePartial", "ProjectCreate", "ProjectDelete",
+    "PurchasedUpdate", "PurchasedUpdatePartial", "PurchasedCreate", "PurchasedBase", "PurchasedDelete", "PurchasedRead",
+    "PurchasedHydroperforatorBase", "PurchasedHydroperforatorUpdate", "PurchasedHydroperforatorRead",
+    "PurchasedHydroperforatorCreate", "PurchasedHydroperforatorDelete", "PurchasedHydroperforatorUpdatePartial",
+    "AdapterAndPlugsUpdate", "AdapterAndPlugsBase", "AdapterAndPlugsRead", "AdapterAndPlugsCreate",
+    "AdapterAndPlugsDelete", "AdapterAndPlugsUpdatePartial",
+    "AccordingToTheDrawBase", "AccordingToTheDrawUpdate", "AccordingToTheDrawCreate", "AccordingToTheDrawDelete",
+    "AccordingToTheDrawRead", "AccordingToTheDrawUpdatePartial"
 )
 
 from .users import UserBase, UserRead, UserUpdatePartial, UserDelete, UserUpdate, UserCreate
 from .metizes import MetizBase, MetizDelete, MetizCreate, MetizUpdatePartial, MetizUpdate, MetizRead, BaseModel
 from .RWD import RWDBase, RWDRead, RWDUpdate, RWDCreate, RWDDelete, RWDUpdatePartial
 from .project import ProjectBase, ProjectRead, ProjectUpdate, ProjectUpdatePartial, ProjectCreate, ProjectDelete
+from .purchased import PurchasedUpdate, PurchasedUpdatePartial, PurchasedCreate, PurchasedBase, PurchasedDelete, \
+    PurchasedRead
+from .purchased_hydroperforator import PurchasedHydroperforatorBase, PurchasedHydroperforatorUpdate, \
+    PurchasedHydroperforatorRead, PurchasedHydroperforatorCreate, PurchasedHydroperforatorDelete, \
+    PurchasedHydroperforatorUpdatePartial
+from .adapters_and_plugs import AdapterAndPlugsUpdate, AdapterAndPlugsBase, AdapterAndPlugsRead, AdapterAndPlugsCreate, \
+    AdapterAndPlugsDelete, AdapterAndPlugsUpdatePartial
+from .according_to_the_drawing import AccordingToTheDrawBase, AccordingToTheDrawUpdate, AccordingToTheDrawCreate, \
+    AccordingToTheDrawDelete, AccordingToTheDrawRead, AccordingToTheDrawUpdatePartial
