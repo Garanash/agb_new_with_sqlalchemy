f814bf988e536cfc3af8db2c7cf7872eedd0ff43
a.dolgov <dolgov_am@mail.ru>
2025-01-28 17:54:15 +0300

    all work on day

--- agb-application/api/api_v1/views/RWD.py
+++ agb-application/api/api_v1/views/RWD.py
@@ -1,16 +1,31 @@
 from typing import Annotated
-
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.responses import RedirectResponse
+from fastapi import APIRouter, Depends, HTTPException, status, Request, Form
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from datetime import datetime
 from api.api_v1.schemas.RWD import RWDCreate, RWDDelete, RWDRead, RWDUpdate, RWDUpdatePartial, RWDBase
 from core.models import db_helper, RWD
 from api.api_v1.crud.CRUD import get_all_objects, create_new_object, update_object, get_object_by_id, delete_object, \
     search_by_request
+from fastapi.templating import Jinja2Templates
 
+templates = Jinja2Templates('templates')
 router = APIRouter(
     tags=['RWDBases'],
 )
+@router.get('/RWDs', response_model_by_alias=True)
+async def get_rwd(
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+        request: Request
+):
+    """
+    вывод всех РВД в таблице
+    :parameter:
+    session: сессия в асинхронную базу данных
+    request: запрос от пользователя
+    """
+    rwd = await get_all_objects(session=session, model=RWD)
+    return templates.TemplateResponse('/search/rwd.html', {"request": request, "rwd": rwd})
 
 
 @router.get('/', response_model_by_alias=True)
@@ -20,13 +35,47 @@ async def get_rwd_item(
     rwd_item = await get_all_objects(session=session, model=RWD)
     return rwd_item
 
+@router.get("/addnew")
+async def add_new_rwd(request: Request):
+    return templates.TemplateResponse("/addnew/add_new_rwd.html",
+                                      {"request": request,
+                                       "current_datetime": datetime.now().strftime("%Y-%m-%dT%H:%M")})
 
-@router.post("/new_rwd_item", response_model=None, response_model_by_alias=True)
-async def create_rwd_item(
+
+@router.post("/patch", response_class= RedirectResponse)
+async def patch_metizes(
+        patch_item: Annotated[RWDUpdatePartial, Form()],
         session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-        rwd_item_create: RWDCreate):
-    rwd_item = await create_new_object(session=session, object_create=rwd_item_create, model=RWD)
-    return rwd_item
+        request: Request):
+    object_for_update = await get_object_by_id(request_id=patch_item.id, session=session, model=RWD)
+    await update_object(session=session, object_for_update=object_for_update, object_updating=patch_item, partial=True)
+    return RedirectResponse("/RWD/RWDs", status_code=301)
+
+
+@router.post("/create", response_model=None, response_model_by_alias=True, response_class=RedirectResponse)
+async def create_rwd(
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+        metiz_create: Annotated[RWDCreate, Form()],
+        request: Request):
+    try:
+        rwd = await create_new_object(session=session, object_create=metiz_create, model=RWD)
+        return RedirectResponse("/RWD/RWDs", status_code=301)
+
+    except BaseException:
+        return templates.TemplateResponse("/search/rwd.html",
+                                          {'request': request, "message": "Такая деталь уже существует"})
+        # return HTTPException(status_code=status.HTTP_409_CONFLICT, detail="rwd already exists")
+
+
+@router.get('/search')
+async def search_metiz_by_request(
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+        request: Request
+):
+    search_item = request.query_params.get('main_input')
+    res_search = await search_by_request(request=search_item, session=session, model=RWD)
+    return templates.TemplateResponse("/finded/rwds.html", {'request': request, "metizes": res_search})
+
 
 
 @router.get('/{rwd_item_id}', response_model=None, response_model_by_alias=True)
@@ -67,9 +116,9 @@ async def update_rwd_item_by_id(
     )
 
 
-@router.get('/s/{request_item}')
-async def search_rwd_by_request(
-        request_item: str,
-        session: Annotated[AsyncSession, Depends(db_helper.session_getter)]
-):
-    return await search_by_request(request=request_item, session=session, model=RWD)
+# @router.get('/s/{request_item}')
+# async def search_rwd_by_request(
+#         request_item: str,
+#         session: Annotated[AsyncSession, Depends(db_helper.session_getter)]
+# ):
+#     return await search_by_request(request=request_item, session=session, model=RWD)
