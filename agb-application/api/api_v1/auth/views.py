7fd8b17d28b66ca44d36b49b0161c02a9419851e
Зольников Юрий <gakusei96@mail.ru>
2025-01-28 10:23:08 +0300

    Adding registration and logout routers

--- agb-application/api/api_v1/auth/views.py
+++ agb-application/api/api_v1/auth/views.py
@@ -8,7 +8,9 @@ from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.templating import Jinja2Templates
 from api.api_v1.auth.functions import search_by_request
-from core.models import db_helper
+from core.models import db_helper, User
+from api.api_v1.crud.CRUD import create_new_object
+from api.api_v1.schemas.users import UserCreate
 from authx import AuthX, AuthXConfig
 
 config = AuthXConfig()
@@ -23,8 +25,10 @@ templates = Jinja2Templates('templates')
 
 security_log = HTTPBasic()
 
+
 def generate_session_id():
     return uuid.uuid4().hex
+
 
 @router.post("/login")
 async def auth_login_cookie(
@@ -38,15 +42,32 @@ async def auth_login_cookie(
         if credentials.password == res.password:
             token = security.create_access_token(uid=res.username)
             response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
-            return templates.TemplateResponse('authuser.html', {"request": request,
-                                                                "response": response,
-                "username": credentials.username,
-                "password": credentials.password
-            })
+            return templates.TemplateResponse('authuser.html',
+                                              {"request": request,
+                                               "response": response,
+                                               "username": credentials.username,
+                                               "password": credentials.password
+                                               })
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid username or password")
 
-# @router.post('/registration')
-# async def registration(
-#         session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-#
-# )
+
+@router.post("/register")
+async def register_user(
+        user: UserCreate,
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)]
+):
+    # Проверяем, существует ли уже пользователь
+    existing_user = await search_by_request(request=user.username, session=session)
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already registered")
+
+    # Создаем нового пользователя
+    await create_new_object(session=session, object_create=user, model=User)
+    return {"message": "User registered successfully"}
+
+
+# Эндпоинт для выхода из системы
+@router.post("/logout")
+async def logout_user(response: Response):
+    response.delete_cookie(config.JWT_ACCESS_COOKIE_NAME)
+    return {"message": "Successfully logged out"}
