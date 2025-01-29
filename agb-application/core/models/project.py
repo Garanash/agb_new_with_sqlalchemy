d4dadb4206a82e920a9554023a259b0bfa8bd4c7
Зольников Юрий <gakusei96@mail.ru>
2025-01-29 14:45:48 +0300

    Adding purchased templates

--- agb-application/core/models/project.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String, Boolean
-from .base import Base
-
-
-class Project(Base):
-    classifier = Column(String(50), nullable=False, unique=True, default='-')
-    project = Column(String(50), nullable=False, unique=True)
-
-    is_active = Column(Boolean)
