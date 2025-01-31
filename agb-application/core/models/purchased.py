3e3a08028ce8412b39c0de3fe40810624990fba7
Зольников Юрий <gakusei96@mail.ru>
2025-01-31 13:11:13 +0300

    Fixing purchased model

--- agb-application/core/models/purchased.py
+++ agb-application/core/models/purchased.py
@@ -7,7 +7,10 @@ class Purchased(Base):
     name_in_catalog = Column(String(50), default='-')
     name_in_catalog_agb = Column(String(50), default='-')
     assigned = Column(String(50), default='-', nullable=False)  # присвоил
+    name_in_KD = Column(String(50), default='-')  # имя в КД
+    name_in_OEM = Column(String(50), default='-')  # имя в OEM
     note = Column(String(50), default='-')  # примечание
+    date = Column(String(50), default='-')  # дата присвоения
     applicability = Column(String(50), default='-')  # применяемость
 
     marked_for_deletion = Column(Boolean, default=False)
