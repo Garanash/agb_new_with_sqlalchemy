a99d23aa62160580d026ca507c71bdfcb0475d29
a.dolgov <dolgov_am@mail.ru>
2025-02-04 17:55:46 +0300

    finaly comma v1.0 fix 12

--- agb-application/docker-entrypoint.sh
+++ agb-application/docker-entrypoint.sh
@@ -14,7 +14,7 @@ wait_for_postgres
 
 # Выполняем миграции
 echo "Выполнение миграций..."
-#alembic revision --autogenerate -m "Initial migration"
+alembic revision --autogenerate -m "Initial migration"
 alembic upgrade head
 
 # Запускаем приложение
