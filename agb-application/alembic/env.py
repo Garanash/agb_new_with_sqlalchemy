5d1f36d09cb3231cd1e7029afef31b5e6a8048c5
a.dolgov <dolgov_am@mail.ru>
2025-02-05 10:56:52 +0300

    finaly comma v1.0 fix 23

--- agb-application/alembic/env.py
+++ agb-application/alembic/env.py
@@ -30,7 +30,7 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-config.set_main_option("sqlalchemy.url", "postgresql+asyncpg://user:password@localhost:5432/agb")
+config.set_main_option("sqlalchemy.url", "postgresql+asyncpg://user:password@172.25.155.25:5432/agb")
 
 
 def run_migrations_offline() -> None:
